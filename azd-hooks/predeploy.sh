13749bd37924f7fac1d7c765a4446359fce5adf3
zhihaoguo <zhihaoguo@microsoft.com>
2024-11-19 15:01:50 +0800

    Refactor Maven commands to use help:evaluate for clarity and efficiency.

--- azd-hooks/predeploy.sh
+++ azd-hooks/predeploy.sh
@@ -8,12 +8,8 @@ export ACR_PASSWORD=$(az acr credential show -n $ACR_NAME -g ${RESOURCE_GROUP_NA
 # Build and push docker image to ACR
 echo "Get image name and version......"
 
-run_maven_command() {
-    mvn -q -Dexec.executable=echo -Dexec.args="$1" --non-recursive exec:exec 2>/dev/null | sed -e 's/\x1b\[[0-9;]*m//g' | tr -d '\r\n'
-}
-
-IMAGE_NAME=$(run_maven_command '${project.artifactId}')
-IMAGE_VERSION=$(run_maven_command '${project.version}')
+IMAGE_NAME=$(mvn help:evaluate "-Dexpression=project.artifactId" -q -DforceStdout)
+IMAGE_VERSION=$(mvn help:evaluate "-Dexpression=project.version" -q -DforceStdout)
 
 echo "Docker build and push to ACR Server ${ACR_SERVER} with image name ${IMAGE_NAME} and version ${IMAGE_VERSION}"
 
